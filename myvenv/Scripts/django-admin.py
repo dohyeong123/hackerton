3af798274315c7d17d02db8ae515f91cc05f2f8f
sudond <sudond@naver.com>
2019-08-09 00:57:04 +0900

    commit

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\sudon\onedrive\바탕 화면\workspace\myhack\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
