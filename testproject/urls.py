f2a62eb2e9694eb2949c40a85e6bc953b5cad984
sudond <sudond@naver.com>
2019-08-08 19:31:57 +0900

    urls

--- testproject/urls.py
+++ testproject/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 import testapp.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',testapp.views.home, name='home'),
-    path('login/',testapp.views.login, name='login'),
-    path('logout/',testapp.views.logout, name='logout'),
-    path('signup/',testapp.views.signup, name='signup'),
-    path('accounts/',include('allauth.urls')),
+    path('', testapp.views.home, name='home'),
+    path('login/', testapp.views.login, name='login'),
+    path('logout/', testapp.views.logout, name='logout'),
+    path('signup/', testapp.views.signup, name='signup'),
+    path('accounts/', include('allauth.urls')),
     
 ]
