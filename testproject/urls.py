357aa36b219c19fc074e671a859c33e9e2249e80
sudond <sudond@naver.com>
2019-08-09 00:49:33 +0900

    commit first

--- testproject/urls.py
+++ testproject/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('login/', testapp.views.login, name='login'),
     path('logout/', testapp.views.logout, name='logout'),
     path('signup/', testapp.views.signup, name='signup'),
+    path('cate_trend/', testapp.views.cate_trend, name='cate_trend'),
     path('accounts/', include('allauth.urls')),
-    
 ]
